4907978011fc8093dcc3ff10d21fb291841baac4
Navish <navishchauhan7@gmail.com>
2020-06-28 21:11:26 -0400

    final commit

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,5 +1,14 @@
 const { new: _new, create, delete: _delete } = require('../controllers/SessionsController');
 
+
+function auth (req, res, next) {
+  if (!req.isAuthenticated()) {
+    req.flash('danger', 'You need to login first.');
+    return res.redirect('/login');
+  }
+  next();
+}
+
 module.exports = router => {
   router.get('/login', _new);
   router.post('/authenticate', create);
